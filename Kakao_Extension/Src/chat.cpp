97920056b20fbfb4d6ddc19072b343bc724ae022
Chang Yeol <ckdduf138@naver.com>
2023-09-15 20:27:58 +0900

    FEAT: 멘션 기능 추가

--- Kakao_Extension/Src/chat.cpp
+++ Kakao_Extension/Src/chat.cpp
@@ -4,39 +4,66 @@
 #include <string>
 
 bool FindKakaoTalk(HWND kako_hWnd);
-void SendChat(HWND kako_hWnd, char* msg);
+void SendChat(HWND kako_hWnd, char* msg, int cnt);
+void GetPersonList();
 
-HWND hEdit, hList, hScrl;
+HWND kakao_hWnd, hEdit, hList, hScrl;
 
 // 카카오톡 최대 입력글자 > 1000글자
 char msg[1000];
 
 int main()
 {
-	HWND kako_hWnd = FindWindow(TEXT("#32770"), 0);
+	kakao_hWnd = FindWindow(TEXT("#32770"), 0);
 
-	while (1) 
+	while (1)
 	{
 		std::cin.getline(msg, 1000);
 
-		if (FindKakaoTalk(kako_hWnd))
+		if (FindKakaoTalk(kakao_hWnd))
 		{
-			SendChat(hEdit, msg);
+			SetForegroundWindow(kakao_hWnd);
+
+			SetForegroundWindow(hEdit);
+
+			int number = 6;
+			for (int idx = 1; idx <= number; idx++)
+			{
+				SendChat(hEdit, msg, idx);
+			}
+
+			Sleep(1000);
+
+			// 엔터 키 누르기
+			INPUT inputs[2];
+			memset(inputs, 0, sizeof(inputs));
+
+			inputs[0].type = INPUT_KEYBOARD;
+			inputs[0].ki.wVk = VK_RETURN; // 엔터 키 코드
+			inputs[0].ki.dwFlags = 0;
+
+			inputs[1].type = INPUT_KEYBOARD;
+			inputs[1].ki.wVk = VK_RETURN; // 엔터 키 코드
+			inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+			SendInput(2, inputs, sizeof(INPUT));
 		}
 		else
 		{
-			kako_hWnd = FindWindow(TEXT("#32770"), 0);
+			kakao_hWnd = FindWindow(TEXT("#32770"), 0);
 		}
+
+		//break;
 	}
 
-	return 0;	
+	return 0;
 }
 
 bool FindKakaoTalk(HWND kako_hWnd)
 {
 	// 카카오톡 채팅창
 	hEdit = FindWindowEx(kako_hWnd, NULL, L"RICHEDIT50W", NULL);
-	
+
 	// 카카오톡 채팅화면
 	hList = FindWindowEx(kako_hWnd, NULL, L"EVA_VH_ListControl_Dblclk", NULL);
 
@@ -57,24 +84,61 @@ bool FindKakaoTalk(HWND kako_hWnd)
 
 }
 
-void SendChat(HWND hEdit, char* msg)
+void GetPersonList()
 {
+	// 특정 키 조합을 눌러서 채팅방의 참가자 목록 열기
+	keybd_event(VK_CONTROL, 0, 0, 0);
+	keybd_event('M', 0, 0, 0);
+	keybd_event('M', 0, KEYEVENTF_KEYUP, 0);
+	keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+}
 
-	SendMessageA(hEdit, WM_SETTEXT, NULL, (LPARAM)msg);
+void SendChat(HWND hEdit, char* msg, int cnt)
+{
+	SetForegroundWindow(kakao_hWnd);
 
-	//int cnt = 3;
-	//while (cnt > 0)
-	//{
-	//	PostMessage(hEdit, WM_KEYDOWN, VK_RIGHT, 0);
-	//	cnt--;
-	//}
+	SetForegroundWindow(hEdit);
 
-	// 전송
-	PostMessage(hEdit, WM_KEYDOWN, VK_RETURN, 0);
-	PostMessage(hEdit, WM_KEYUP, VK_RETURN, 0);
+	SendMessage(hEdit, EM_SETSEL, -1, -1);
+	Sleep(300);
 
+	SendMessageA(hEdit, EM_REPLACESEL, true, (LPARAM)"@");
+	Sleep(300);
 
-	// 전송
-	//PostMessage(hEdit, WM_KEYDOWN, VK_RETURN, 0);
-	//PostMessage(hEdit, WM_KEYUP, VK_RETURN, 0);
+	PostMessage(hEdit, WM_KEYDOWN, VK_RIGHT, 0);
+	Sleep(300);
+
+	for (int idx = 0; idx < cnt; idx++)
+	{
+		// 아래키 누르기
+		INPUT inputs[2];
+		memset(inputs, 0, sizeof(inputs));
+
+		inputs[0].type = INPUT_KEYBOARD;
+		inputs[0].ki.wVk = VK_DOWN; // 아래 키 코드
+		inputs[0].ki.dwFlags = 0;
+
+		inputs[1].type = INPUT_KEYBOARD;
+		inputs[1].ki.wVk = VK_DOWN; // 아래 키 코드
+		inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+		SendInput(2, inputs, sizeof(INPUT));
+		Sleep(300);
+	}
+
+	Sleep(500);
+
+	// 엔터 키 누르기
+	INPUT inputs[2];
+	memset(inputs, 0, sizeof(inputs));
+
+	inputs[0].type = INPUT_KEYBOARD;
+	inputs[0].ki.wVk = VK_RETURN; // 엔터 키 코드
+	inputs[0].ki.dwFlags = 0;
+
+	inputs[1].type = INPUT_KEYBOARD;
+	inputs[1].ki.wVk = VK_RETURN; // 엔터 키 코드
+	inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+	SendInput(2, inputs, sizeof(INPUT));
 }
